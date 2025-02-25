c0fc1dd1d08efb0622bf0d8ff5c7bf75950e77c9
splitzq <werinq.real@gmail.com>
2025-02-25 12:44:22 +0100

    updated colors, added simple animations

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ def home():
 def portfolio():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('notfound.html'), 404
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
 document.getElementById("portfolioButton").addEventListener("click", function() {
     alert("button clicked!");
 });
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.onload = async function() {
+    const mainContainers = document.getElementsByClassName('mainContainer')
+
+    if (mainContainers.length > 0) {
+        const mainContainer = mainContainers[0]
+        mainContainer.classList.add("fade-in")
+    }
+
+}
